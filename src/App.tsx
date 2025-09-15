4866bdbb6da7719b5ccb3e95c1da2793ef6ffda4
Ishagi-Yoichi <nikunjkr1752003@gmail.com>
2025-09-15 19:40:25 +0530

    Portfolio initalization

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import Home from './Home'
+import './App.css'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.tsx
@@ -0,0 +1,63 @@
+export default function Home(){
+
+    return(
+        <><div className="flex-col justify-center">
+            <div className="h-[100vh] w-full flex justify-center p-3 lg:p-5"> 
+                <div className="h-[100vh] w-full flex justify-center rounded-2xl overflow-hidden">
+                    <div className="flex md:hidden">
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div> <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+            
+                    </div>
+                    <div className="hidden md:flex">
+                    <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div> <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+                        <div className="h-full w-[40px] flex shadow-2xl bg-gradient-to-b from-slate-400 via-slate-800 to-black"></div>
+            
+                    </div>
+
+                </div>
+                <div className="absolute"></div>
+            </div>
+        </div>
+        </>
+    )
+}
